37113fa0928dabdfdd6108ad9b0c2358fb87c7f4
Dev-Nei <dev-nei@hotmail.com>
2021-05-27 19:11:56 -0300

    Funcionalidade alteração de senha

--- BancoDedados_Senha.php
+++ BancoDedados_Senha.php
@@ -3,11 +3,9 @@
 $erro = null;
 $valido = false;
 
-// Abrir com os dados lidos para alterar
-
 try
 {
-    $connection = new PDO("mysql:host=localhost;dbname=cursophp", "root", "123456");
+    $connection = new PDO("mysql:host=localhost;dbname=cursophp", "root", "senhadobanco");
     $connection->exec("set names utf8");
 }
 catch(PDOException $e)
@@ -16,22 +14,21 @@ catch(PDOException $e)
     exit();
 }
 
-
-
-
 if(isset($_REQUEST["validar"]) && $_REQUEST["validar"] == true)
 {
     if($_POST["senha"] != $_POST["senhaRepete"])
     {
         $erro = "Senhas digitadas diferentes";
-    }   
+        $erro .= "<BR><A href='?id=".$_POST["id"]."'>Tentar novamente</A>";
+        echo $erro;
+        exit();
+    }
     else
     {
         $valido = true;
-        
-              
+
         $sql = "UPDATE usuarios SET
-                senha = ?               
+                senha = ?
                 WHERE id = ?";
                 
         $stmt = $connection->prepare($sql);
@@ -53,32 +50,30 @@ if(isset($_REQUEST["validar"]) && $_REQUEST["validar"] == true)
 else
 {
     $rs = $connection->prepare("SELECT nome, email FROM usuarios WHERE id = ?");
-    $rs ->bindParam(1, $_REQUEST["id"]);
-
+    $rs->bindParam(1, $_REQUEST["id"]);
+    
     if($rs->execute())
     {
         if($registro = $rs->fetch(PDO::FETCH_OBJ))
         {
             $_POST["nome"] = $registro->nome;
-            $_POST["email"] = $registro->email;               
+            $_POST["email"] = $registro->email;
         }
         else
         {
-            $erro = "Registro  não encontrado";
+            $erro = "Registro não encontrado";
         }
     }
     else
     {
         $erro = "Falha na captura do registro";
     }
-
-    
 }
 
 ?>
 <HTML>
     <HEAD>
-        <TITLE>Banco de Dados: Alteração de senha</TITLE>
+        <TITLE>Banco de Dados: Alteração de Senha</TITLE>
     </HEAD>
     <BODY>
         <?php
@@ -101,22 +96,23 @@ else
         <FORM method=POST action="?validar=true">
 
             Nova senha para o usuário
-             <?php
+            <?php
                 echo $_POST["nome"];
-                echo " (".$_POST["email"].")" ;
-                echo "<BR>";             
-             ?>
-
-             <BR>
-             Digite a senha:
-             <INPUT type=PASSWORD name=senha><BR>    
-
-             <BR>
-             Digite a senha novamente:        
-             <INPUT type=PASSWORD name=senhaRepete><BR>
-   
-            <INPUT type=HIDDEN name=id 
-                   value="<?php echo $_REQUEST["id"]; ?>">
+                echo " (".$_POST["email"].")";
+                echo "<BR>";
+            ?>
+            
+            <BR>
+            Digite a senha:
+            <INPUT type=PASSWORD name=senha><BR>
+            
+            <BR>
+            Digite a senha novamente:
+            <INPUT type=PASSWORD name=senhaRepete><BR>
+            
+            <INPUT type=HIDDEN name=id
+                   value="<?php echo $_REQUEST["id"]; ?>"
+            >
                 
             <INPUT type=SUBMIT value="Alterar">
 
