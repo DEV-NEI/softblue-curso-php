595a7f6a6a8c221357f5f6f7d6de6dc7feaf527f
Dev-Nei <dev-nei@hotmail.com>
2021-05-28 18:09:49 -0300

    Classes e Objetos

---
+++ OO_ClassesObjetos.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OO: Classes e Objetos</title>
+</head>
+<body>
+
+<?php
+
+    class Carro
+    {
+        private $velocidade;
+        private $cor;
+
+        public function __construct($cor)
+        {
+            $this->setCor($cor);
+            $this->setVelocidade(0);
+        }
+
+        public function getVelocidade()
+        {
+            return $this->velocidade;
+        }
+
+        public function getCor()
+        {
+            return $this->cor;
+        }
+
+        private function setVelocidade($velocidade)
+        {
+            if($velocidade > 110 || $velocidade < 0)
+            {
+                echo "Velocidade não permitida<BR>";
+            }
+            else
+            {
+                $this->velocidade = $velocidade;
+            }
+        }
+
+        public function setCor($cor)
+        {
+            $this->cor = $cor;
+        }
+
+        public function acelerar()
+        {
+            $this->setVelocidade($this->getVelocidade() + 1);
+        }
+
+        public function frear()
+        {
+            $this->setVelocidade($this->getVelocidade() - 1);
+        }
+    }
+
+    // $meuCarro = new Carro();
+
+    // $meuCarro->velocidade = 40;
+    // $meuCarro->cor = "Preta";
+
+   // echo "O carro de cor " . $meuCarro->cor . " está andando: " . $meuCarro->velocidade;
+   // echo "<BR>";
+
+    $meuCarro = new Carro("Vermelha");
+    $meuCarro->acelerar();
+    $meuCarro->acelerar();
+    $meuCarro->acelerar();
+    $meuCarro->acelerar();
+    $meuCarro->acelerar();      
+    $meuCarro->frear();
+    $meuCarro->frear();
+    $meuCarro->acelerar();
+    $meuCarro->acelerar();    
+
+      echo "O carro de cor " . $meuCarro->getCor() . " está andando: " . $meuCarro->getVelocidade() . "Km/h";
+   echo "<BR>";
+
+  // $meuCarro->setVelocidade(70);
+  // echo "O carro de cor " . $meuCarro->getCor() . " está andando: " . $meuCarro->getVelocidade();
+  // echo "<BR>";
+
+  // $meuCarro->setVelocidade(-130);
+  // echo "O carro de cor " . $meuCarro->getCor() . " está andando: " . $meuCarro->getVelocidade();
+  // echo "<BR>";
+   
+
+  // $meuCarro->cor = "Amarela";
+  // $meuCarro->velocidade = "220";
+
+  // echo "O carro de cor " . $meuCarro->cor . " está andando: " . $meuCarro->velocidade;
+  // echo "<BR>";
+?>
+    
+</body>
+</html>
