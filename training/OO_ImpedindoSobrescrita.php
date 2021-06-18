31471b308bebb8b0fc8360b1050859635fc58890
Dev-Nei <dev-nei@hotmail.com>
2021-06-18 20:05:36 -0300

    Impedindo sobrescrita do metodo

---
+++ training/OO_ImpedindoSobrescrita.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OO: Sobrescrevendo métodos</title>
+</head>
+<body>
+
+<?php
+    
+    class Mamifero
+    {
+        final public function andar()
+        {
+            echo "andar(mamifero) <BR>";
+        }
+    }
+
+    // O uso do final impede o sobrescrita do metodo.
+/*
+    class Homem extends Mamifero
+    {
+        public function andar()
+        {   echo "andar(homem) <BR>";
+
+
+        }
+    }
+    */
+
+    $m = new Mamifero();
+    $m->andar();
+
+    /*
+    $h= new Homem();
+    $h->andar();
+    */
+
+?>
+    
+</body>
+</html>
